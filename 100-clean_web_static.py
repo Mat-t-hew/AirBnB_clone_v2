73b9556e4060ae3fe7c2c00fe2b7b375d636d81d
Matthew <122832898+Mat-t-hew@users.noreply.github.com>
2024-03-03 19:05:39 +0200

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -14,15 +14,17 @@ def do_clean(number=0):
     number is 2, keeps the most and second-most recent archives,
     etc.
     """
-    number = 1 if int(number) == 0 else int(number)
+    number = int(number)
+    if number < 1:
+        number = 1
 
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
     with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+        local_archives = sorted(os.listdir("."))
+        archives_to_delete_local = local_archives[:-number]
+        [local("rm -f {}".format(archive)) for archive in archives_to_delete_local]
 
     with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+        run_archives = run("ls -tr").split()
+        web_static_archives = [archive for archive in run_archives if "web_static_" in archive]
+        archives_to_delete_remote = web_static_archives[:-number]
+        [run("rm -rf {}".format(archive)) for archive in archives_to_delete_remote]
